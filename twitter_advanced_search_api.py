ce522e0cf2d7b2bc504cafc95447a15282e899b8
covid <dev@covid19-mentions-db.com>
2020-04-14 12:52:28 +0500

    coordinates fix

--- twitter_advanced_search_api.py
+++ twitter_advanced_search_api.py
@@ -62,7 +62,7 @@ def extract_results_from_json(j, query):
                 _location = calculate_centroid_for_twitter_bounding_box(
                     tweet['place']['bounding_box']['coordinates'][0]
                 )
-                location['coordinates'] = [_location[0], _location[1]]
+                location['coordinates'] = [_location[1], _location[0]]
                 location['country'] = tweet['place']['country']
             except:
                 pass
